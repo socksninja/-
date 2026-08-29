b583108dc6f46916a2277baa56194ab020efcda7
socksninja <1164752614@qq.com>
2026-08-29 19:35:24 +0800

    fix: enable static export for mainland deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  reactStrictMode: true,
+};
 module.exports = nextConfig;
